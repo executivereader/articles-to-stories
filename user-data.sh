a8ac26921736e055015f9bcda559b26ae0880e9a
executivereader <executivereader@users.noreply.github.com>
2015-08-10 16:48:27 -0400

    added openblas, numpy, scipy dependencies

--- user-data.sh
+++ user-data.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # the contents of this file should be put in the user data field
 sudo apt-get update
-sudo apt-get install -y awscli git python-dev python-pip
+sudo apt-get install -y awscli git python-dev python-pip libopenblas-dev
 sudo mkfs -t ext4 /dev/xvdf
 sudo mkdir /data
 sudo mount /dev/xvdf /data
@@ -12,5 +12,6 @@ sudo cp /home/ubuntu/mongo-startup/connection_string.txt /home/ubuntu/articles-t
 sudo cp /home/ubuntu/mongo-startup/update_replica_set.py /home/ubuntu/articles-to-stories/update_replica_set.py
 cd /home/ubuntu/articles-to-stories
 sudo wget https://s3.amazonaws.com/word2vec-googlenews/GoogleNews-vectors-negative300.bin.gz
+sudo pip install numpy scipy
 sudo pip install gensim pymongo
 sudo screen -dm bash -c "sudo python articles-to-stories.py"
