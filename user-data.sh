98f33fff6c81b0b350b725fb6a73ac19f56e749f
executivereader <executivereader@users.noreply.github.com>
2015-08-10 15:56:45 -0400

    added important stuff here

--- user-data.sh
+++ user-data.sh
@@ -1,12 +1,16 @@
 #!/usr/bin/env bash
 # the contents of this file should be put in the user data field
 sudo apt-get update
-sudo apt-get install -y git python-dev python-pip
+sudo apt-get install -y awscli git python-dev python-pip
+sudo mkfs -t ext4 /dev/xvdf
+sudo mkdir /data
+sudo mount /dev/xvdf /data
 cd /home/ubuntu/
 sudo git clone https://github.com/executivereader/mongo-startup.git
 sudo git clone https://github.com/executivereader/articles-to-stories.git
 sudo cp /home/ubuntu/mongo-startup/connection_string.txt /home/ubuntu/articles-to-stories/connection_string.txt
 sudo cp /home/ubuntu/mongo-startup/update_replica_set.py /home/ubuntu/articles-to-stories/update_replica_set.py
 cd /home/ubuntu/articles-to-stories
-sudo pip install pymongo
+sudo wget https://s3.amazonaws.com/word2vec-googlenews/GoogleNews-vectors-negative300.bin.gz
+sudo pip install gensim pymongo
 sudo screen -dm bash -c "sudo python articles-to-stories.py"
