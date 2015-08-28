362bf053b492f7d97f3fce534bd6c36d96ffc0e6
executivereader <executivereader@users.noreply.github.com>
2015-08-28 17:22:15 -0400

    ok let's try the old parameters!

--- articles-to-stories.py
+++ articles-to-stories.py
@@ -182,7 +182,7 @@ def cluster_docs(docs,client,collectionname = None,filename = None):
     try:
         cluster_model = pickle.loads(modelstore.get_version(filename=filename).read())
     except NoFile:
-        cluster_model = cluster.DBSCAN(eps=1.0,min_samples=1)
+        cluster_model = cluster.DBSCAN(eps=0.5,min_samples=5)
     training_data = []
     for doc in docs:
         try:
